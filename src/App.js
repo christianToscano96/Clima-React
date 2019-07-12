b9ff5deecf3dbe2d7703428cce0615ef42970b8d
Christian Toscano <chrismarcadopago@gmail.com>
2019-07-12 19:50:31 -0300

    creacion de los componentes header y formulario, haciendo que se tomen los campos de los inputs en el state con hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
-
+//componentes
+import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
+
+  const datosConsulta = datos => {
+    console.log(datos);
+  }
+
   return (
-    <h1>hola</h1>
+    <div className="App">
+      <Header 
+      titulo="Clima con React"
+      />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m6">
+              <Formulario 
+                datosConsulta={datosConsulta}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,61 @@
+import React, { useState} from 'react';
+
+function Formulario({datosConsulta}) {
+
+    // state del componente
+    //busqueda = state, guardarBusqeda = this.setState([])
+    const [busqueda, guardarBusqueda] = useState({
+        ciudad : '',
+        pais : ''
+    })
+
+    const handleChange = e => {
+        //cambiar el state
+        guardarBusqueda({
+           ...busqueda,
+           [e.target.name] : e.target.value 
+        });
+        
+    }
+
+    const consultarClima = e => {
+        e.preventDefault();
+
+        //pasar hacia el componente principal la busqueda del usuario
+        datosConsulta(busqueda);
+    }
+
+    return(
+        <form
+            onSubmit={consultarClima}
+        >
+            <div className="input-field col s12">
+                <input 
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    onChange={handleChange}
+                />
+                <label htmlFor="ciudad">Ciudad: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <select onChange={handleChange} name="pais">
+                    <option value="seleccione un paìs"></option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">Mèxico</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="PE">Peru</option>
+                    <option value="ES">España</option>
+                </select>
+            </div>
+
+            <div className="input-field col s12">
+                <input type="submit" className="waves-effect waves-light btn-large btn-block yellow accent-4" value="Buscar CLima"/>
+            </div>
+        </form>
+    )
+}
+
+export default Formulario;
